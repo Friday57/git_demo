2881e7d75e74b650b631262ba2cb83f039696543
friday <571352008@qq.com>
2022-03-30 14:59:58 +0800

    hot-fix commit1

--- src/main/java/com/itguigu/git_test.java
+++ src/main/java/com/itguigu/git_test.java
@@ -5,5 +5,6 @@ public class git_test {
         System.out.println("hello.git");
         System.out.println("hello.git");
         System.out.println("hello.git");
+        System.out.println("hello.git hot-fix");
     }
 }
