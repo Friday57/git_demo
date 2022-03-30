f49daf3cf6514ae58eb27cd9f0b289efc7d049be
friday <571352008@qq.com>
2022-03-30 14:43:55 +0800

    second commit

--- src/main/java/com/itguigu/git_test.java
+++ src/main/java/com/itguigu/git_test.java
@@ -3,5 +3,6 @@ package com.itguigu;
 public class git_test {
     public static void main(String[] args) {
         System.out.println("hello.git");
+        System.out.println("hello.git");
     }
 }
